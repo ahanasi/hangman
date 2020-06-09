8a829b0f85004b1ba8c72376d68face4396fe5a5
Ahana <ahanasi@seas.upenn.edu>
2020-06-09 14:39:46 -0700

    Record wrong guesses in missed array

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,18 +4,19 @@ require_relative "secret_word.rb"
 require "pry"
 
 class Hangman
-  attr_accessor :player_guess, :word, :display
+  attr_reader :word, :guesses_left, :player_guess, :word, :display, :misses
 
   def initialize()
     @player_guess = Player.new().guess
     @word = SecretWord.new().word
     @display = Display.new().blank_slate(@word)
     @guesses_left = 6
+    @misses = []
   end
 
   def ask_player_for_guess()
     #Ask player for guess
-    puts "Guess a letter in the secret word!"
+    puts "\nGuess a letter in the secret word!\n"
     @player_guess = gets.chomp.upcase
   end
 
@@ -25,7 +26,7 @@ class Hangman
 
   def update_display()
     display_array = @display.split(" ")
-    @word.split(//).each_with_index do |char,idx|
+    @word.split(//).each_with_index do |char, idx|
       if char == @player_guess
         display_array[idx] = @player_guess
       end
@@ -37,15 +38,35 @@ class Hangman
     @word == @display.gsub(/\s+/, "")
   end
 
+  def play_round()
+    ask_player_for_guess()
+    puts "\nYour guess: #{@player_guess}"
+    if correct_guess?() && @display.split(" ").include?(@player_guess)
+      puts "Uh-oh! You lose a try for repeating a previous guess."
+      @guesses_left -= 1
+    elsif correct_guess?()
+      update_display()
+    else
+      puts "Wrong guess!"
+      @misses.push(@player_guess)
+      @misses = @misses.uniq
+      @guesses_left -= 1
+    end
+
+    puts "Tries left: #{@guesses_left}"
+    puts "Misses: #{@misses.join(", ")}"
+  end
 end
 
 test = Hangman.new()
-puts test.word
-puts test.display
-test.ask_player_for_guess
 
+until test.victory? || (test.guesses_left == 0)
+  puts test.display
+  test.play_round
+end
 
-puts "Your guess: #{test.player_guess}"
-puts "Was your guess correct? #{test.correct_guess?()}"
-
-puts test.update_display
+if test.victory?
+  puts "#{test.display}\nYou won!"
+else
+  puts "You lost!\nThe secret word was #{test.word}"
+end
