d68136789fd7883ef9db419103eb7b3133ef37b4
Ahana <ahanasi@seas.upenn.edu>
2020-06-09 11:15:12 -0700

    Initialize word from dictionary

---
+++ lib/secret_word.rb
@@ -0,0 +1,12 @@
+class SecretWord
+  attr_accessor :word
+
+  def initialize()
+    @word = File.readlines("5desk.txt", chomp: true).select {|word| (5..12).cover? word.length}.sample
+  end
+
+ 
+end
+
+test = SecretWord.new()
+puts test.word
